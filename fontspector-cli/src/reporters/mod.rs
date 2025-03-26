11d1a12b3d30b87cebf09e5f8c4bdba38b70a6ce
Simon Cozens <simon@simon-cozens.org>
2025-03-26 16:10:46 +0000

    Make clippy happier

--- fontspector-cli/src/reporters/mod.rs
+++ fontspector-cli/src/reporters/mod.rs
@@ -142,14 +142,12 @@ pub fn create_user_home_templates_directory(force: bool) -> PathBuf {
 
 pub(crate) fn any_stdout(args: &Args) -> Result<bool, String> {
     let yes_stdout = Some("-".to_string());
-    let count_stdout = if args.json == yes_stdout { 1 } else { 0 }
+    let count_stdout: usize = if args.json == yes_stdout { 1 } else { 0 }
         + (if args.csv == yes_stdout { 1 } else { 0 })
         + if args.ghmarkdown == yes_stdout { 1 } else { 0 };
-    if count_stdout > 1 {
-        Err("Only one of --json, --csv, or --ghmarkdown can be stdout".to_string())
-    } else if count_stdout == 1 {
-        Ok(true)
-    } else {
-        Ok(true)
+    match count_stdout {
+        1 => Ok(true),
+        0 => Ok(false),
+        _ => Err("Only one of --json, --csv, or --ghmarkdown can be stdout".to_string()),
     }
 }
