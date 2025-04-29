8372ea194eb3a21fb074e415e2eab0e8f0d721b0
Simon Cozens <simon@simon-cozens.org>
2025-04-29 17:38:38 +0100

    Bake it in

--- profile-iso15008/src/lib.rs
+++ profile-iso15008/src/lib.rs
@@ -7,7 +7,6 @@ pub struct Iso15008;
 impl fontspector_checkapi::Plugin for Iso15008 {
     fn register(&self, cr: &mut Registry) -> Result<(), String> {
         let builder = ProfileBuilder::new()
-            .include_profile("universal")
             .add_section("Iso15008 Fonts Checks")
             .add_and_register_check(checks::iso15008::intercharacter_spacing)
             .add_and_register_check(checks::iso15008::interline_spacing)
@@ -17,6 +16,3 @@ impl fontspector_checkapi::Plugin for Iso15008 {
         builder.build("iso15008", cr)
     }
 }
-
-#[cfg(not(target_family = "wasm"))]
-pluginator::plugin_implementation!(fontspector_checkapi::Plugin, Iso15008);
